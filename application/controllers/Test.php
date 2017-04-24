393f4c40af8947b5dbf73333751a20284eb53c3a
aneeshnirguna <aneeshnirguna90@gmail.com>
2017-04-24 18:33:45 +0530

    mongo insert

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -8,11 +8,41 @@ class Test extends CI_Controller {
 	public function index()
 	{
 		//echo phpinfo();
-		$m = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-		$db = $m->selectDB("newschunk");
-		
-	  		$connection=  new MongoClient($db['connection_string'],array('username'=>$db['username'],'password'=>$db['password']));
-			echo $connection;
+		$m = new MongoDB\Driver\Manager("mongodb://localhost:27017/test");
+		//$m = new MongoDB\Driver\Manager("mongodb://ncadmin:aneesh123@localhost:27017/nc");
+		//$db = $m->selectDB("newschunk");
+		$bulk = new MongoDB\Driver\BulkWrite;
+		$bulk->insert(['title' => "test title", 'content' => 'test content',
+		'cdate' => '24-april-2017',
+		'mdate' => '24-april-2017',
+			'userInfo' => ['email' => 'test@test.com','uname' => 'aneesh','phone' => '9999999999'],
+			'category' => 'entertainment',
+			'comments' => 'test comments',
+			'likes' => '123',
+			'tags' => 'trendy',
+		]);
+		$m->executeBulkWrite('nc.test', $bulk);
+		// $command = new MongoDB\Driver\Command([
+    	// 	'title' => 'test title',
+		// 	'content' => 'test content',
+		// 	'cdate' => '24-april-2017',
+		// 	'mdate' => '24-april-2017',
+		// 	'userInfo' => ['email' => 'test@test.com','uname' => 'aneesh','phone' => '9999999999'],
+		// 	'category' => 'entertainment',
+		// 	'comments' => 'test comments',
+		// 	'likes' => '123',
+		// 	'tags' => 'trendy',
+    	// 	//'content' => [
+        // 	//['$group' => ['_id' => '$y', 'sum' => ['$sum' => '$x']]],
+		// 	//],
+    	// 	'cursor' => new stdClass,
+		// 	]);
+		// $cursor = $m->executeCommand('test', $command);
+	  	//	$connection=  new MongoClient($db['connection_string'],array('username'=>$db['username'],'password'=>$db['password']));
+			//var_dump($m);
+			// foreach ($cursor as $document) {
+   			// 	 var_dump($document);
+			// 	}
 
 	}
 }
